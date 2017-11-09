f653cb77ffc8cf52ab8ea8d19d890b3c28616554
Трушенков Дмитрий <diman28051999@gmail.com>
2017-11-09 16:37:30 +0300

    Initial commit

---
+++ src/ru/tds/eggsorchiken/EggOrChiken.java
@@ -0,0 +1,9 @@
+package ru.tds.eggsorchiken;
+
+/**
+ * Класс, в котором реализовано решение спора: "Что возникло раньше яйцо или курица?"
+ *
+ * @author Trushenkov Dmitry 15ИТ18
+ */
+public class EggOrChiken {
+}
