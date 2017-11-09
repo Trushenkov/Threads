dc45329ec6317f16bb69df10f98c29d8101b42c4
Трушенков Дмитрий <diman28051999@gmail.com>
2017-11-10 01:28:22 +0300

    Выполнен рефакторинг, излечены именнованные константы.

--- src/ru/tds/catchingthreads/FirstThread.java
+++ src/ru/tds/catchingthreads/FirstThread.java
@@ -6,17 +6,23 @@ package ru.tds.catchingthreads;
  * @author Trushenkov Dmitry 15ИТ18
  */
 public class FirstThread extends Thread {
+
+    public static final int COUNT_OF_STEPS = 2000;
+    public static final int TIME_OF_DELAY = 5;
+    public static final int MIN_PRIORITY = 1;
+    public static final int MAX_PRIORITY = 10;
+    public static final int STEPS_TO_CHANGE_PRIORITY = 500;
+
     public void run() {
-        Thread thread = Thread.currentThread();
-        thread.setPriority(1);
-        for (int i = 0; i < 2000; i++) {
+        setPriority(MIN_PRIORITY);
+        for (int i = 0; i < COUNT_OF_STEPS; i++) {
             try {
-                sleep(5);
+               sleep(TIME_OF_DELAY);
             } catch (InterruptedException e) {
             }
-            System.out.println("Первый поток " + i + " км.");
-            if (i == 500) {
-                thread.setPriority(10);
+            System.out.println("Первый поток " + i );
+            if (i == STEPS_TO_CHANGE_PRIORITY) {
+                setPriority(MAX_PRIORITY);
             }
         }
     }
--- src/ru/tds/catchingthreads/SecondThread.java
+++ src/ru/tds/catchingthreads/SecondThread.java
@@ -6,17 +6,21 @@ package ru.tds.catchingthreads;
  * @author Trushenkov Dmitry 15ИТ18
  */
 public class SecondThread extends Thread{
+    public static final int COUNT_OF_STEPS = 2000;
+    public static final int TIME_OF_DELAY = 5;
+    public static final int MIN_PRIORITY = 1;
+    public static final int MAX_PRIORITY = 10;
+    public static final int STEPS_TO_CHANGE_PRIORITY = 650;
     public void run() {
-        Thread thread = Thread.currentThread();
-        thread.setPriority(10);
-        for (int i = 0; i < 2000; i++) {
+        setPriority(MAX_PRIORITY);
+        for (int i = 0; i < COUNT_OF_STEPS; i++) {
             try {
-                thread.sleep(5);
+                sleep(TIME_OF_DELAY);
             } catch (InterruptedException e) {
             }
-            System.out.println("Второй поток " + i + " км.");
-            if (i == 700) {
-                thread.setPriority(1);
+            System.out.println("Второй поток " + i);
+            if (i == STEPS_TO_CHANGE_PRIORITY) {
+                setPriority(MIN_PRIORITY);
             }
         }
     }
