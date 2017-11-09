1aa54ae94dd74529afeee77bf9e6fd6b38d3377b
Trushenkov Dmitry <diman28051999@gmail.com>
2017-11-09 16:50:48 +0300

    Deleted file.

--- src/ru/tds/eggsorchiken/EggOrChiken.java
+++
@@ -1,9 +0,0 @@
-package ru.tds.eggsorchiken;
-
-/**
- * Класс, в котором реализовано решение спора: "Что возникло раньше яйцо или курица?"
- *
- * @author Trushenkov Dmitry 15ИТ18
- */
-public class EggOrChiken {
-}
