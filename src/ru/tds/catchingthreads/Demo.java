47a82e7c40e31c85e04d0f27cc544231505be887
Trushenkov Dmitry <diman28051999@gmail.com>
2017-12-08 21:34:36 +0300

    Изменены комментарии к классу.

--- src/ru/tds/catchingthreads/Demo.java
+++ src/ru/tds/catchingthreads/Demo.java
@@ -1,17 +1,16 @@
 package ru.tds.catchingthreads;
 
 /**
- * Класс, в котором созданы и запущены два потока, демонстрирующие динамическое изменение приоритетов двух потоков.
+ * Класс, в котором созданы и запущены два потока.
  *
  * @author Trushenkov Dmitry 15ИТ18
  */
 public class Demo {
     public static void main(String[] args) {
-        Threads thread1 = new Threads(1,10);
-        Threads thread2 = new Threads(10,1);
+        CatchingThread thread1 = new CatchingThread(1,10);
+        CatchingThread thread2 = new CatchingThread(10,1);
 
         thread1.start();
         thread2.start();
-
     }
 }
