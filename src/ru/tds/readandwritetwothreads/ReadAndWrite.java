ef6f2fba1ff2505ec66730be066978f90e384b9f
Trushenkov Dmitry <diman28051999@gmail.com>
2017-12-19 18:19:14 +0300

    Метод Main извлечен в отдельный класс.

--- src/ru/tds/readandwritetwothreads/ReadAndWrite.java
+++ src/ru/tds/readandwritetwothreads/ReadAndWrite.java
@@ -3,28 +3,35 @@ package ru.tds.readandwritetwothreads;
 import java.io.*;
 
 /**
- * Класс, в котором реализовано многопоточное считывание данных из файлов из запись в результирующий файл.
+ * Класс, в котором реализовано многопоточное считывание данных из файлов и запись в результирующий файл.
  *
  * @author Trushenkov Dmitry 15ИТ18
  */
 public class ReadAndWrite extends Thread {
-    private String way;
-    private static volatile BufferedWriter bufferedWriter;
 
-    public ReadAndWrite(final String way) {
+    private String way;
+
+    private static volatile BufferedWriter writer;
+
+    ReadAndWrite(String way, BufferedWriter writer) {
         this.way = way;
+        ReadAndWrite.writer = writer;
     }
 
     public void run() {
+
         long before = System.currentTimeMillis();
         String string;
+
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(way))) {
             while ((string = bufferedReader.readLine()) != null) {
                 writeString(string);
                 yield();
             }
         } catch (IOException e) {
+            e.printStackTrace();
         }
+
         long after = System.currentTimeMillis();
         System.out.println((after - before) % 1000);
     }
@@ -35,25 +42,10 @@ public class ReadAndWrite extends Thread {
      * @param string строка, которую нужно записать в файл
      * @throws IOException исключение
      */
-    public static synchronized void writeString(String string) throws IOException {
-        bufferedWriter.write(string + "\n");
+    private static synchronized void writeString(String string) throws IOException {
+
+        writer.write(string + "\n");
+
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        bufferedWriter = new BufferedWriter(new FileWriter("src\\ru\\tds\\readandwritetwothreads\\Результат.txt", false));
-
-        ReadAndWrite thread1 = new ReadAndWrite("src\\ru\\tds\\readandwritetwothreads\\1 файл.txt");
-        ReadAndWrite thread2 = new ReadAndWrite("src\\ru\\tds\\readandwritetwothreads\\2 файл.txt");
-
-        thread1.start();
-        thread2.start();
-        if (thread1.isAlive()) {
-            thread1.join();
-        }
-        if (thread2.isAlive()) {
-            thread2.join();
-        }
-
-        bufferedWriter.close();
-    }
 }
