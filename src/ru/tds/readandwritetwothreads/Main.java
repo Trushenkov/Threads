45bb21d4aacc0cddc000de9ee02d4214a9cf6dd3
Trushenkov Dmitry <diman28051999@gmail.com>
2017-12-19 18:22:32 +0300

    Метод Main извлечен из класса ReadAndWrite.

--- src/ru/tds/readandwritetwothreads/Main.java
+++ src/ru/tds/readandwritetwothreads/Main.java
@@ -1,4 +1,34 @@
 package ru.tds.readandwritetwothreads;
 
+import java.io.*;
+
+/**
+ * Класс, в котором создаются и запускаютя два потока, которые параллельно читают
+ * данные из двух разных текстовных файлов и записывают построчно в реультирующий файл.
+ *
+ * @author Трушенков Дмитрий 15ИТ18
+ */
 public class Main {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        BufferedWriter writer = new BufferedWriter(new FileWriter("src\\ru\\tds\\readandwritetwothreads\\Результат.txt", false));
+
+        ReadAndWrite thread1 = new ReadAndWrite("src\\ru\\tds\\readandwritetwothreads\\1 файл.txt", writer);
+        ReadAndWrite thread2 = new ReadAndWrite("src\\ru\\tds\\readandwritetwothreads\\2 файл.txt", writer);
+
+        thread1.start();
+        thread2.start();
+
+        if (thread1.isAlive()) {
+            thread1.join();
+        }
+
+        if (thread2.isAlive()) {
+            thread2.join();
+        }
+
+        writer.close();
+
+    }
 }
